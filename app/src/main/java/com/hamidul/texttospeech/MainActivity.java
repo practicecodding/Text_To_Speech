bb04aeecea9cfaeb1edd88a1917fbab32f5f7376
user <practicecoddin@gmail.com>
2023-09-08 15:52:11 +0600

    Text To Speech

--- app/src/main/java/com/hamidul/texttospeech/MainActivity.java
+++ app/src/main/java/com/hamidul/texttospeech/MainActivity.java
@@ -3,13 +3,83 @@ package com.hamidul.texttospeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    private SeekBar sPitch,sSpeed;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        sSpeed = findViewById(R.id.speed);
+        sPitch = findViewById(R.id.pitch);
+
+        textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if (status == TextToSpeech.SUCCESS){
+                    int result  = textToSpeech.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("TTS","Language not supported");
+                    }else {
+                        button.setEnabled(true);
+                    }
+                }else {
+                    Log.e("TTS","Initialization failed");
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+
+
+    }
+    private void speak(){
+        String Text = editText.getText().toString();
+
+        float pitch =(float) sPitch.getProgress()/50;
+        if (pitch<0) pitch = 0.1f;
+
+        float speed =(float) sSpeed.getProgress()/50;
+        if (speed<0) speed = 0.1f;
+
+        textToSpeech.setPitch(pitch);
+        textToSpeech.setSpeechRate(speed);
+
+        textToSpeech.speak(Text,TextToSpeech.QUEUE_FLUSH,null,null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }
 
